9ac2dce8f5bebd2fdc2fc22ef29ce0d65e3a509a
Cihan Emre Kısakurek <cekisakurek@gmail.com>
2012-07-16 14:32:55 +0300

    Initial Commit

---
+++ AmbientNotification/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AmbientNotification
+//
+//  Created by Cihan Emre Kisakurek on 7/16/12.
+//  Copyright (c) 2012 ustat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
